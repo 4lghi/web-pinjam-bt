dfdf1f8a6430abcfaf753fd651f1526537cbf2af
108_Muhammad Al Ghifari Lubis <115900989+4lghi@users.noreply.github.com>
2025-06-11 05:40:34 +0700

    protect frontend routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./ui/Login";
 import DashboardAdmin from "./ui/DashboardAdmin";
-import SidebarAdmin from "./ui/components/SidebarAdmin";
 import DaftarPeminjaman from "./ui/DaftarPeminjaman";
 import DaftarPengajuan from "./ui/DaftarPengajuan";
 import DaftarPengguna from "./ui/DaftarPengguna";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboardAdmin" element={<DashboardAdmin />} />
-      <Route path="/daftarPeminjaman" element={<DaftarPeminjaman />} />
-      <Route path="/daftarPengajuan" element={<DaftarPengajuan />} />
-      <Route path="/daftarPengguna" element={<DaftarPengguna />} />
+
+      <Route
+        path="/dashboardAdmin"
+        element={<ProtectedRoute element={DashboardAdmin} allowedRoles={["admin"]} />}
+      />
+
+      <Route
+        path="/daftarPeminjaman"
+        element={<ProtectedRoute element={DaftarPeminjaman} allowedRoles={["admin"]} />}
+      />
+
+      <Route
+        path="/daftarPengajuan"
+        element={<ProtectedRoute element={DaftarPengajuan} allowedRoles={["admin"]} />}
+      />
+
+      <Route
+        path="/daftarPengguna"
+        element={<ProtectedRoute element={DaftarPengguna} allowedRoles={["admin"]} />}
+      />
     </Routes>
   );
 }
